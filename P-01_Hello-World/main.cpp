8527f4f39bcb145ea5f2792ea953203d02092315
techblummen <dan_eugenio@hotmail.com>
2018-01-18 17:19:23 -0200

    Code completion

--- P-01_Hello-World/main.cpp
+++ P-01_Hello-World/main.cpp
@@ -1,11 +1,15 @@
 //This is the first project based on the online course
 //"C++ tutorial for complete beginners" on Udemy
 
+//In this project: basic output
+
 #include <iostream>
 using namespace std;
 
 int main(){
 
+    cout << "Don't stop the " << "PARTY" << flush;
+    cout << "!" << " :)" << endl;
 
     return 0;
 }
