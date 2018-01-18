85b54d2dd47d7c5181ace751cffaa13b2a0b8001
techblummen <dan_eugenio@hotmail.com>
2018-01-18 16:58:35 -0200

    P-01 Create files
    I am creating the first file of project P-01_Hello-World

---
+++ P-01_Hello-World/main.cpp
@@ -0,0 +1,11 @@
+//This is the first project based on the online course
+//"C++ tutorial for complete beginners" on Udemy
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
