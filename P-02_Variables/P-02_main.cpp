f625ba20cfc62a9fa0c98fcf9df50e10890c7ee7
techblummen <dan_eugenio@hotmail.com>
2018-01-18 17:42:05 -0200

    P-02 Code completion

--- P-02_Variables/P-02_main.cpp
+++ P-02_Variables/P-02_main.cpp
@@ -6,7 +6,21 @@ using namespace std;
 
 int main() {
 
+    int numberFingers = 10;
+    int numberToes = 10;
+    int numberTotal = numberFingers + numberToes;
+    int newFingers = 1;
 
+    cout << "Original status" << endl;
+    cout << "Number of fingers: " << numberFingers << endl;
+    cout << "Number of toes: " << numberToes << endl;
+    cout << "Total: " << numberTotal << endl;
+    cout << endl;
+    cout << newFingers << " new finger(s) found!" << endl;
+    numberFingers = numberFingers + newFingers;
+    cout << "New number of fingers: " << numberFingers << endl;
+    numberTotal = numberFingers + numberToes;
+    cout << "New total: " << numberTotal << endl;
 
     return 0;
 }
