ccdec235b1264e3b6b7c0d1bb5f36f2b326d2316
techblummen <dan_eugenio@hotmail.com>
2018-01-18 17:55:26 -0200

    P-03 File completion

--- P-03_Strings/P-03_main.cpp
+++ P-03_Strings/P-03_main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 int main() {
 
+    string str1 = "My name is ";
+    string myName = "Gaga";
+    string str2 = "My name is not ";
+    string notMyName = "John";
+
+    cout << str1 << myName << endl;
+    cout << str2 + notMyName << endl;
+
+    //Notice that we can concatenate string variables
+    //in the output, using 2 different strategies
 
     return 0;
 }
