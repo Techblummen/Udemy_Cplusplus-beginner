d9c0147e8609ab50ec2c70c8a807b9845225e7d9
techblummen <dan_eugenio@hotmail.com>
2018-01-18 18:06:20 -0200

    P-04 File completion

--- P-04_Input/P-04_main.cpp
+++ P-04_Input/P-04_main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 
 int main() {
 
+    string text;
+    cout << "Who are you?" << endl << "Please write your name here:" << flush;
+    cin >> text;
+    cout << "Nice to meet you, " << text << "!" << endl;
+    cout << endl;
+
+    int age;
+    cout << "How old are you" << endl << "Please write your age here: " << flush;
+    cin >> age;
+    cout << "So you are: " << age << " years old. Great!" << endl;
+
 
     return 0;
 }
