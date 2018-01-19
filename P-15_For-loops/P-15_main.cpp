634b504981af18cf46d9409cfff3c1fcd8d9ab94
techblummen <dan_eugenio@hotmail.com>
2018-01-19 14:43:39 -0200

    P-15 Source file creation and completion, and compiled files

---
+++ P-15_For-loops/P-15_main.cpp
@@ -0,0 +1,19 @@
+//P-15_For-loops
+//In this project: working with for loops
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i=0; i<10; i++){
+        cout << "Step " << i << endl;
+    }
+
+    //The for loop is very useful and this is apparently only the most common way of using this loop.
+	//It is nice you can create the counting variable (i), establish its limits and add the increment
+	//all within one line of code.
+
+    return 0;
+}
+
