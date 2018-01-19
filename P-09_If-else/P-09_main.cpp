5f0bc079173f71d29e75ccd17cac470eddbc96ae
techblummen <dan_eugenio@hotmail.com>
2018-01-19 12:49:33 -0200

    File completion

--- P-09_If-else/P-09_main.cpp
+++ P-09_If-else/P-09_main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 
 int main() {
 
+    int choice;
+
+    cout << "1.\tBanana" << endl;
+    cout << "2.\tApple" << endl;
+    cout << "3.\tOrange" << endl;
+    cout << "4.\tGrape" << endl;
+    cout << "5.\tLettuce" << endl;
+    cout << "Which of the above options is NOT a fruit?" << endl;
+    cout << "Write the corresponding number here: " << flush;
+
+    cin >> choice;
+
+    if (choice < 5){
+        cout << "Wrong answer." << endl;
+    } else {
+        cout << "Correct answer!" << endl;
+    }
 
     return 0;
 }
