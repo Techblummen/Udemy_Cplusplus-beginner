3c10cb8a30336a65ec59febed5149dde98ff420a
techblummen <dan_eugenio@hotmail.com>
2018-01-19 13:58:28 -0200

    P-10 File Completion

--- P-10_If-else-if/P-10_main.cpp
+++ P-10_If-else-if/P-10_main.cpp
@@ -6,6 +6,32 @@ using namespace std;
 
 int main() {
 
+    int choice;
+
+    cout << "1.\tBanana" << endl;
+    cout << "2.\tApple" << endl;
+    cout << "3.\tOrange" << endl;
+    cout << "4.\tGrape" << endl;
+    cout << "5.\tLettuce" << endl;
+    cout << "Which of the above options is NOT a fruit?" << endl;
+    cout << "Write the corresponding number here: " << flush;
+
+    cin >> choice;
+    cout << endl;
+
+    if (choice == 1){
+        cout << "Banana is a fruit. Wrong answer." << endl;
+    } else if (choice == 2){
+        cout << "Apple is a fruit. Wrong answer." << endl;
+    } else if (choice == 3){
+        cout << "Orange is a fruit. Wrong answer." << endl;
+    } else if (choice == 4){
+        cout << "Grape is a fruit. Wrong answer." << endl;
+    } else if (choice == 5){
+        cout << "Correct answer!" << endl;
+    } else {
+        cout << "Invalid option..." << endl;
+    }
 
     return 0;
 }
