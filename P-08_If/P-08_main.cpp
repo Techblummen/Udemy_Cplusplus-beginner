a2e737dfe0c949fd463a35f5b74b99b48e9f31b5
techblummen <dan_eugenio@hotmail.com>
2018-01-19 12:37:56 -0200

    File completion

--- P-08_If/P-08_main.cpp
+++ P-08_If/P-08_main.cpp
@@ -6,6 +6,33 @@ using namespace std;
 
 int main() {
 
+    string password = "mojo";
+    string input;
+    cout << "What is the password?" << endl << "Please write it here: " << flush;
+    cin >> input;
+    cout << "'" << input << "'" << endl;
+
+    if (input == password) {
+        cout << "Congrats! You are right!" << endl;
+    }
+    if (input != password) {
+        cout << "Sorry, this is not the right password" << endl;
+    }
+    cout << endl;
+
+    int code = 1234;
+    int input2;
+    cout << "What is the code?" << endl << "Please write it here: " << flush;
+    cin >> input2;
+
+    if (input2 == code) {
+        cout << "Congrats! You are right!" << endl;
+    }
+    if (input2 != code) {
+        cout << "Sorry, this is not the right code" << endl;
+        cout << code << endl;
+        cout << input2 << endl;
+    }
 
     return 0;
 }
